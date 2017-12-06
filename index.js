e623c176f858f3ee5e9b3bb9fdf81fe8ba3fc4f6
ramchandra <ramchandra@ramchandras-Mac-mini.local>
2017-12-06 12:47:55 +0530

    Redux installed

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import { AppRegistry } from 'react-native';
+import React from 'react';
 import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-AppRegistry.registerComponent('ViaFarm', () => App);
+// Import the reducer and create a store
+const store = createStore(reducer)
+
+// Pass the store into the Provider
+const AppWithStore = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent('ViaFarm', () => AppWithStore);
+
+// Initial state of the store
+const initialState = {
+    todos: ['Click to remove', 'Learn React Native', 'Write Code', 'Ship App'],
+}
+
+function reducer(state = initialState, action) {
+    return state
+}
 
 
 /// npm install
@@ -9,3 +31,4 @@ AppRegistry.registerComponent('ViaFarm', () => App);
 // DROP_DOWN >>> npm i react-native-modal-dropdown -S
 // ROUTER_FLUX >>> npm i react-native-router-flux --save
 // MATERIAL_TEXTFIELD >>> npm install --save react-native-material-textfield
+// REACT_REDUX >>> npm install --save react-redux
